9fff26bf529df52ea69ddc0aa99749173f95cfb9
Sergio <sergio.pena1.mt@correo.usa.edu.co>
2021-10-25 20:52:07 -0500

    first commit

---
+++ src/main/java/tutoria/domingo/interfaces/InterfaceOrtesis.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package tutoria.domingo.interfaces;
+
+import org.springframework.data.repository.CrudRepository;
+import tutoria.domingo.modelo.Ortesis;
+
+/**
+ *
+ * @author Sergio Peña
+ */
+public interface InterfaceOrtesis extends CrudRepository<Ortesis,Integer>
+{
+    
+}
---
+++ src/main/java/tutoria/domingo/interfaces/InterfaceReservacion.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package tutoria.domingo.interfaces;
+
+import org.springframework.data.repository.CrudRepository;
+import tutoria.domingo.modelo.Reservacion;
+
+/**
+ *
+ * @author Sergio Peña
+ */
+public interface InterfaceReservacion extends CrudRepository<Reservacion,Integer>
+{
+    
+}
